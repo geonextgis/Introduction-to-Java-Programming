ad131ee50fab52c645a7896ec00d625204d908ca
Krish Halder <geonextgis@gmail.com>
2024-07-23 20:41:21 +0530

    First Commit.

---
+++ src/CWH_04_literals.java
@@ -0,0 +1,14 @@
+public class CWH_04_literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66d;
+        long ageDino = 56859586069l;
+        boolean a = true;
+        System.out.println(a);
+        String firstName = "Krishnagopal";
+        String lastName = "Halder";
+        System.out.println(firstName);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,36 @@
+// package com.company;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//     // write your code here
+//         System.out.println("Hello World");
+//         int harry;
+
+//     }
+// }
+
+// //  Write a java program to add three numbers
+// public class Main {
+
+//     public static void main(String[] args) {
+//         System.out.println("The sum of these number is: ");
+//         int num1 = 6;
+//         int num2 = 10;
+//         int num3 = 15;
+//         int sum = num1 + num2 + num3;
+//         System.out.println(sum);
+//     }
+// }
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The sum of three numbers is: ");
+        int num1 = 3;
+        int num2 = 6;
+        int num3 = 8;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
+
